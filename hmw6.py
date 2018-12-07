d4e2b9d75fd2f6bb3fc693c1cab658c3214a3293
rsrocki01 <45692158+rsrocki01@users.noreply.github.com>
2018-12-07 08:53:05 -0600

    Add files via upload

---
+++ hmw6.py
@@ -0,0 +1,354 @@
+#6.1
+person = {
+    'first_name': 'eric',
+    'last_name': 'matthes',
+    'age': 43,
+    'city': 'sitka',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+#6.2
+favorite_numbers = {
+    'mandy': 42,
+    'micah': 23,
+    'gus': 7,
+    'hank': 1000000,
+    'maggie': 0,
+    }
+
+num = favorite_numbers['mandy']
+print("Mandy's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['micah']
+print("Micah's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['gus']
+print("Gus's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['hank']
+print("Hank's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['maggie']
+print("Maggie's favorite number is " + str(num) + ".")
+#6.3
+glossary = {
+    'string': 'A series of characters.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'list': 'A collection of items in a particular order.',
+    'loop': 'Work through a collection of items, one at a time.',
+    'dictionary': "A collection of key-value pairs.",
+    }
+
+word = 'string'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'comment'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'list'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'loop'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'dictionary'
+print("\n" + word.title() + ": " + glossary[word])
+#6.4
+glossary = {
+    'string': 'A series of characters.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'list': 'A collection of items in a particular order.',
+    'loop': 'Work through a collection of items, one at a time.',
+    'dictionary': "A collection of key-value pairs.",
+    'key': 'The first item in a key-value pair in a dictionary.',
+    'value': 'An item associated with a key in a dictionary.',
+    'conditional test': 'A comparison between two values.',
+    'float': 'A numerical value with a decimal component.',
+    'boolean expression': 'An expression that evaluates to True or False.',
+    }
+
+for word, definition in glossary.items():
+    print("\n" + word.title() + ": " + definition)
+#6.6
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+        language.title() + ".")
+
+print("\n")
+
+coders = ['phil', 'josh', 'david', 'becca', 'sarah', 'matt', 'danielle']
+for coder in coders:
+    if coder in favorite_languages.keys():
+        print("Thank you for taking the poll, " + coder.title() + "!")
+    else:
+        print(coder.title() + ", what's your favorite programming language?")
+#6.8
+# Make an empty list to store the pets in.
+pets = []
+
+# Make individual pets, and store each one in the list.
+pet = {
+    'animal type': 'python',
+    'name': 'john',
+    'owner': 'guido',
+    'weight': 43,
+    'eats': 'bugs',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'chicken',
+    'name': 'clarence',
+    'owner': 'tiffany',
+    'weight': 2,
+    'eats': 'seeds',
+}
+pets.append(pet)
+
+pet = {
+    'animal type': 'dog',
+    'name': 'peso',
+    'owner': 'eric',
+    'weight': 37,
+    'eats': 'shoes',
+}
+pets.append(pet)
+
+# Display information about each pet.
+for pet in pets:
+    print("\nHere's what I know about " + pet['name'].title() + ":")
+    for key, value in pet.items():
+        print("\t" + key + ": " + str(value))
+#6.10
+favorite_numbers = {
+    'mandy': [42, 17],
+    'micah': [42, 39, 56],
+    'gus': [7, 12],
+    }
+
+for name, numbers in favorite_numbers.items():
+    print("\n" + name.title() + " likes the following numbers:")
+    for number in numbers:
+        print("  " + str(number))
+#7.1
+car = input("What kind of car would you like? ")
+
+print("Let me see if I can find you a " + car.title() + ".")
+#7.2
+party_size = input("How many people are in your dinner party tonight? ")
+party_size = int(party_size)
+
+if party_size > 8:
+    print("I'm sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready.")
+#7.3
+number = input("Give me a number, please: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(str(number) + " is a multiple of 10.")
+else:
+    print(str(number) + " is not a multiple of 10.")
+#7.4
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    topping = input(prompt)
+    if topping != 'quit':
+        print("  I'll add " + topping + " to your pizza.")
+    else:
+        break
+#7.8
+sandwich_orders = ['veggie', 'grilled cheese', 'turkey', 'roast beef']
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print("I'm working on your " + current_sandwich + " sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich + " sandwich." )
+#7.10
+name_prompt = "\nWhat's your name? "
+place_prompt = "If you could visit one place in the world, where would it be? "
+continue_prompt = "\nWould you like to let someone else respond? (yes/no) "
+
+# Responses will be stored in the form {name: place}.
+responses = {}
+
+while True:
+    # Ask the user where they'd like to go.
+    name = input(name_prompt)
+    place = input(place_prompt)
+
+    # Store the response.
+    responses[name] = place
+
+    # Ask if there's anyone else responding.
+    repeat = input(continue_prompt)
+    if repeat != 'yes':
+        break
+
+# Show results of the survey.
+print("\n--- Results ---")
+for name, place in responses.items():
+    print(name.title() + " would like to visit " + place.title() + ".")
+#8.1
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+display_message()
+#8.2
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('The Abstract Wild')
+#8.4
+def make_shirt(size='large', message='I love Python!'):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
+#8.6
+def city_country(city, country):
+    """Return a string like 'Santiago, Chile'."""
+    return(city.title() + ", " + country.title())
+
+city = city_country('santiago', 'chile')
+print(city)
+
+city = city_country('ushuaia', 'argentina')
+print(city)
+
+city = city_country('longyearbyen', 'svalbard')
+print(city)
+#8.8
+def make_album(artist, title, tracks=0):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+    }
+    if tracks:
+        album_dict['tracks'] = tracks
+    return album_dict
+
+
+# Prepare the prompts.
+title_prompt = "\nWhat album are you thinking of? "
+artist_prompt = "Who's the artist? "
+
+# Let the user know how to quit.
+print("Enter 'quit' at any time to stop.")
+
+while True:
+    title = input(title_prompt)
+    if title == 'quit':
+        break
+
+    artist = input(artist_prompt)
+    if artist == 'quit':
+        break
+
+    album = make_album(artist, title)
+    print(album)
+
+print("\nThanks for responding!")
+#8.10
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
+    great_magicians = []
+
+    # Make each magician great, and add it to great_magicians.
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    # Add the great magicians back into magicians.
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\n")
+make_great(magicians)
+show_magicians(magicians)
+#8.12
+def make_sandwich(*items):
+    """Make a sandwich with the given items."""
+    print("\nI'll make you a great sandwich:")
+    for item in items:
+        print("  ...adding " + item + " to your sandwich.")
+    print("Your sandwich is ready!")
+
+make_sandwich('roast beef', 'cheddar cheese', 'lettuce', 'honey dijon')
+make_sandwich('turkey', 'apple slices', 'honey mustard')
+make_sandwich('peanut butter', 'strawberry jam')
+#8.14
+def make_car(manufacturer, model, **options):
+    """Make a dictionary representing a car."""
+    car_dict = {
+        'manufacturer': manufacturer.title(),
+        'model': model.title(),
+        }
+    for option, value in options.items():
+        car_dict[option] = value
+
+    return car_dict
+
+my_outback = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(my_outback)
+
+my_accord = make_car('honda', 'accord', year=1991, color='white',
+        headlights='popup')
+print(my_accord)
+#8.15
+"""Functions related to printing 3d models."""
+
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until there are none left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+
+        # Simulate creating a 3d print from the design.
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
